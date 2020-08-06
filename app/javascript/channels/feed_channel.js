5ee7f34bc6295fc7b3fd7804d614866967ef52a4
allezz <aerbay@gmail.com>
2020-08-06 22:48:26 +0200

    Fix html, Set feed_channel for cableready

--- app/javascript/channels/feed_channel.js
+++ app/javascript/channels/feed_channel.js
@@ -1,15 +1,8 @@
-import consumer from "./consumer"
+import consumer from "./consumer";
+import CableReady from "cable_ready";
 
 consumer.subscriptions.create("FeedChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
-
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
+    if (data.CableReady) CableReady.perform(data.operations);
+  },
 });
