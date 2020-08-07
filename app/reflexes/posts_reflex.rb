199fb17d8f4a1d101e8182ac4327b3f393b63fdd
allezz <aerbay@gmail.com>
2020-08-07 19:19:23 +0200

    Add cable ready to post reflex actions

--- app/reflexes/posts_reflex.rb
+++ app/reflexes/posts_reflex.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
 class PostsReflex < ApplicationReflex
+  include CableReady::Broadcaster
+  include ActionView::RecordIdentifier
   def repost
     post = Post.find(element.dataset[:id])
     post.increment! :reposts_count
+    cable_ready['feed'].text_content(
+      selector: "##{dom_id(post, 'reposts')}",
+      text: post.reposts_count
+    )
+    cable_ready.broadcast
   end
 
   def like
     post = Post.find(element.dataset[:id])
     post.increment! :likes_count
+    cable_ready['feed'].text_content(
+      selector: "##{dom_id(post, 'likes')}",
+      text: post.likes_count
+    )
+    cable_ready.broadcast
   end
 end
